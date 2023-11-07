0d887b2b1441d2e55454d024d5908bd46921685c
MustaphaRam <mustapha.mad.75@gmail.com>
2023-11-07 13:37:35 +0100

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ get the number_of_subscribers function
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ get the number_of_subscribers function
 
 import requests
 
+
 def top_ten(subreddit):
     """Return the top 10 hottes posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
@@ -12,9 +13,10 @@ def top_ten(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
-        "limit":10
+        "limit": 10
     }
-    response = requests.get(url, headers=headers,params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return 0
